c16ac76cb486b25fa5683e0027aec9fb2bd24304
Borislav Godumov <96978166+khanark@users.noreply.github.com>
2023-11-12 18:18:26 +0100

    Add files via upload

---
+++ news-homepage-main/app.js
@@ -0,0 +1,28 @@
+const navBtn = document.querySelector(".btn-nav");
+const mobileNav = document.querySelector(".mobile-nav-wrapper");
+
+navBtn.addEventListener("click", (e) => {
+    mobileNav.classList.toggle("show");
+    e.currentTarget.classList.toggle("close");
+});
+
+mobileNav.addEventListener("click", (e) => {
+    const nav = e.target;
+    if (
+        !nav.classList.contains("mobile-nav-wrapper") &&
+        !nav.classList.contains("show")
+    )
+        return;
+    mobileNav.classList.toggle("show");
+    navBtn.classList.toggle("close");
+});
+
+document.querySelector(".menu-toggle").addEventListener(
+    "change",
+    (e) => {
+        e.currentTarget.checked
+            ? (document.documentElement.style.overflowY = "hidden")
+            : (document.documentElement.style.overflowY = "");
+    },
+    false
+);
